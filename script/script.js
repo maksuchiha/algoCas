545195c6c1e9c9a72a1f89212daf639d6bf0e3ec
Maksim Sheko <uchihaboy95@gmail.com>
2022-01-13 02:06:22 +0300

    fix bugs css in project

--- script/script.js
+++ script/script.js
@@ -4,22 +4,17 @@
     const modalActive = document.querySelector('.problems-item_purple')
     const modal = document.querySelector('.modal')
     const modalClose = document.querySelector('.modal__close')
-    const gtnGoCasino = document.querySelector('.go-casino')
+
 
     const modalToggle = () => {
         modal.classList.toggle('modal_active')
         document.body.classList.toggle('overf_active')
     }
 
-    const showBtnCasino = () => {
-        if (window.scrollY > 640) {
-            gtnGoCasino.classList.add('go-casino_active')
-        } else {
-            gtnGoCasino.classList.remove('go-casino_active')
-        }
-    }
-
-    modalActive.addEventListener('click', modalToggle)
+    modalActive.addEventListener('click', (e) => {
+        e.preventDefault()
+        modalToggle()
+    })
     modalClose.addEventListener('click', modalToggle)
     window.addEventListener('scroll', showBtnCasino)
 })()
