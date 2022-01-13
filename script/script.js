4bbcf5d26eb08f09c91e230ce3d8d658d0972ebb
Maksim Sheko <uchihaboy95@gmail.com>
2022-01-13 04:43:08 +0300

    fix bugs css in project

--- script/script.js
+++ script/script.js
@@ -4,11 +4,14 @@
     const modalActive = document.querySelector('.problems-item_purple')
     const modal = document.querySelector('.modal')
     const modalClose = document.querySelector('.modal__close')
+    const mainContainer = document.querySelector('body')
+    const casinoBtn = document.querySelector('.go-casino')
 
 
     const modalToggle = () => {
         modal.classList.toggle('modal_active')
-        document.body.classList.toggle('overf_active')
+        mainContainer.classList.toggle('active')
+        casinoBtn.classList.toggle('go-casino_active')
     }
 
     modalActive.addEventListener('click', (e) => {
@@ -16,6 +19,5 @@
         modalToggle()
     })
     modalClose.addEventListener('click', modalToggle)
-    window.addEventListener('scroll', showBtnCasino)
 })()
 
